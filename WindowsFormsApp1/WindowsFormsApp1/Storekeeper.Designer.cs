76d58c9f402453502cd16b8aae3cc51268c66124
semenova <admin@user.com>
2020-05-07 11:55:32 +0300

    Change the forms desingn "Storekeeper"

--- WindowsFormsApp1/WindowsFormsApp1/Storekeeper.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Storekeeper.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(629, 280);
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(652, 288);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(88, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Sign out";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,17 +48,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(326, 29);
+            this.label1.BackColor = System.Drawing.Color.Thistle;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label1.Location = new System.Drawing.Point(278, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.Size = new System.Drawing.Size(218, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "Экран кладовщика";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(259, 67);
+            this.button2.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(270, 82);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(241, 23);
+            this.button2.Size = new System.Drawing.Size(241, 28);
             this.button2.TabIndex = 2;
             this.button2.Text = "Список тканей";
             this.button2.UseVisualStyleBackColor = true;
@@ -65,9 +70,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(259, 116);
+            this.button3.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(270, 170);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(241, 23);
+            this.button3.Size = new System.Drawing.Size(241, 29);
             this.button3.TabIndex = 3;
             this.button3.Text = "Спикой фурнитуры";
             this.button3.UseVisualStyleBackColor = true;
@@ -77,6 +83,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(765, 341);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
