96500324273ad31687b8b7c01de8a59a1a77caf3
semenova <admin@user.com>
2020-05-07 12:01:55 +0300

    Change the desingn "Klient"

--- WindowsFormsApp1/WindowsFormsApp1/Klient.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Klient.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(611, 287);
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(593, 297);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(99, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "Sign out";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,16 +46,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(289, 26);
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(263, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.Size = new System.Drawing.Size(197, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "Экран заказчика";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Klient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(722, 344);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
--- WindowsFormsApp1/WindowsFormsApp1/Klient.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Klient.cs
@@ -24,5 +24,10 @@ namespace WindowsFormsApp1
             Form1.FORMA.Show();
             this.Close();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
